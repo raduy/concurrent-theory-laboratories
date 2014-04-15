b2d79f7f46c287611e54c967870fb7ef28a7fecf
raduj.lukasz@gmail.com <raduj.lukasz@gmail.com>
2014-04-15 22:24:32 +0200

    Dinning philosophers problem with semaphores.

--- src/main/java/lab02/diningphilosophers/Dinner.java
+++ src/main/java/lab02/diningphilosophers/Dinner.java
@@ -4,25 +4,28 @@ import lab02.BinarySemaphore;
 import lab02.Semaphore;
 
 /**
+ * Runner for dinning philosophers problem.
+ *
  * @author Lukasz Raduj <raduj.lukasz@gmail.com>
  */
 public class Dinner {
+
     public static void main(String[] args) {
         final BinarySemaphore[] forks = new BinarySemaphore[5];
+
         for (int i = 0; i < 5; i++) {
             forks[i] = new BinarySemaphore();
         }
 
-        final Semaphore butler = new Semaphore(4);
+        final Semaphore butler = new Semaphore(5);
 
         Thread[] philosophers = new Thread[5];
         for (int i = 0; i < 5; i++) {
-            philosophers[i] = new Thread(new Philosopher(i, forks, butler));
+            philosophers[i] = new Thread(new Philosopher(forks, butler));
         }
 
         for (Thread philosopher : philosophers) {
             philosopher.start();
         }
-
     }
 }
--- src/main/java/lab02/diningphilosophers/Philosopher.java
+++ src/main/java/lab02/diningphilosophers/Philosopher.java
@@ -2,48 +2,58 @@ package lab02.diningphilosophers;
 
 import lab02.BinarySemaphore;
 import lab02.Semaphore;
+import org.apache.log4j.Logger;
 
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 /**
+ * Class represents philosopher for five dining philosophers problem.
+ *
  * @author Lukasz Raduj <raduj.lukasz@gmail.com>
  */
 public class Philosopher implements Runnable {
+    private static final Logger LOGGER = Logger.getLogger(Philosopher.class.getCanonicalName());
+
     private final static Random random = new Random();
+    private static int nextID = 1;
     private final int id;
     private final BinarySemaphore[] forks;
     private final Semaphore butler;
 
-    public Philosopher(int id, BinarySemaphore[] forks, Semaphore butler) {
-        this.id = id;
+    public Philosopher(BinarySemaphore[] forks, Semaphore butler) {
+        this.id = nextID++;
         this.forks = forks;
         this.butler = butler;
     }
 
     private synchronized void think() throws InterruptedException {
-        System.out.println("I'm " + id + "th philosopher and I'm thinking now.");
-        wait(random.nextInt(5000));
+        LOGGER.info("I'm " + id + "th philosopher and I'm thinking now.");
+
+        TimeUnit.SECONDS.sleep(random.nextInt(5));
     }
 
     private synchronized void eat() throws InterruptedException {
-        butler.down();
-        forks[id].down();
-        forks[(id + 1) % 5].down();
-        System.out.println("I'm " + id + "th and I'm going to eat sth.");
-        forks[id].up();
-        forks[(id + 1) % 5].up();
-        butler.up();
+        butler.take();
+        forks[id % 5].take();
+        forks[(id + 1) % 5].take();
 
-        wait(random.nextInt(5000));
+        /* tabs for better readability */
+
+        LOGGER.info("\t\t\t\t\t\t\t I'm " + id + "th and I'm going to eat sth.");
+        TimeUnit.SECONDS.sleep(random.nextInt(5));
+
+        forks[id % 5].release();
+        forks[(id + 1) % 5].release();
+        butler.release();
     }
-
 
     @Override
     public void run() {
-        while (true) {
+        while (!Thread.currentThread().isInterrupted()) {
             try {
-                think();
                 eat();
+                think();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
